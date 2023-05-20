96a0972d7dab6f1012dfb229153d025b1be79d03
chainman2209 <chainsawman2209@gmail.com>
2023-05-20 16:11:20 +0700

    feat: update dashboard layout

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,12 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import React, { FC } from "react";
+
+const page: FC = () => {
+  const session = getServerSession(authOptions);
+  if (!session) notFound();
+  return <div>page</div>;
+};
+
+export default page;
